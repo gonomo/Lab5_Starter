7de3f4f5df05591ad2420931396bda7655044310
gonomo <yoyo-ghreeb@hotmail.com>
2022-10-27 16:54:50 -0700

    j n
    n

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,24 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  let image = document.querySelector('img');
+  let select = document.getElementById('horn-select');
+  let audio = document.getElementsByClassName("hidden")[0];
+  let changeImg = function() {
+    if (select.value == 'air-horn') {
+      image.src = "assets/images/air-horn.svg";
+      audio.src = "assets/audio/air-horn.mp3";
+    }
+    else if (select.value == 'car-horn') {
+      image.src = "assets/images/car-horn.svg";
+      audio.src = "assets/audio/car-horn.mp3";
+    }
+    else if (select.value == 'party-horn') {
+      image.src = "assets/images/party-horn.svg";
+      audio.src = "assets/audio/party-horn.mp3";
+    }
+  }
+  select.addEventListener('change', changeImg);
+  
 }
+init();
