1d4ceb41acd2e7f9b618f01acf859cf8f1adc5e4
gonomo <yoyo-ghreeb@hotmail.com>
2022-10-27 17:46:16 -0700

    third comment

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,6 +3,28 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
+  let scale = document.getElementById("volume");
+  let soundImg = document.getElementById("volume-controls").querySelector("img");
+  let button = document.querySelector("button");
+  let pressButton = function() {
+    audio.play();
+  }
+  let changeSound = function() {
+    if(scale.value == 0) {
+      soundImg.src = "assets/icons/volume-level-0.svg";
+    }
+    else if(scale.value >= 1 && scale.value < 33) {
+      soundImg.src = "assets/icons/volume-level-1.svg";
+    }
+    else if(scale.value >= 33 && scale.value < 67) {
+      soundImg.src = "assets/icons/volume-level-2.svg";
+    }
+    else if(scale.value >= 67) {
+      soundImg.src = "assets/icons/volume-level-3.svg";
+    } 
+    audio.volume = (scale.value / 100.0);
+  }
+
   let image = document.querySelector('img');
   let select = document.getElementById('horn-select');
   let audio = document.getElementsByClassName("hidden")[0];
@@ -21,6 +43,8 @@ function init() {
     }
   }
   select.addEventListener('change', changeImg);
+  scale.addEventListener('change', changeSound);
+  button.addEventListener('click', pressButton);
   
 }
 init();
