631823b66be830da618d075bd8737e2072286fca
gonomo <yoyo-ghreeb@hotmail.com>
2022-10-30 17:21:17 -0700

    explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,35 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  let select = document.querySelector("select");
+  let list = (window.speechSynthesis).getVoices();
+  // Populate the "Select Voice" dropdown
+  for(let i = 0; i < list.length; i++) {
+    let option = new Option(list[i].name, i);
+    select.add(option, undefined);
+  }
+  // Select which voice
+  let utterance = new SpeechSynthesisUtterance();
+  let changeVoice = function() {
+    utterance.voice = list[select.value];
+  }
+  select.addEventListener("change", changeVoice);
+
+  // Play voice when "Press to Talk" is pressed
+  let textArea = document.querySelector("textarea");
+  let button = document.querySelector("button");
+  let img = document.querySelector("img");
+
+  let startSpeaking = function() {
+    utterance.text = textArea.value;
+    img.src = "assets/images/smiling-open.png";
+    window.speechSynthesis.speak(utterance);
+  }
+  let endSpeaking = function() {
+    img.src = "assets/images/smiling.png";
+    window.speechSynthesis.cancel();
+  }
+  button.addEventListener("click", startSpeaking);
+  utterance.addEventListener("end", endSpeaking);
 }
+init();
